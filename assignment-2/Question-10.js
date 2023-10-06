2a4581303c3c4e269724a6b44ef49c92ca59ffda
munendra-successive <munendra.kushwaha@successive.tech>
2023-10-06 14:41:51 +0530

    Modified

--- assignment-2/Question-10.js
+++ assignment-2/Question-10.js
@@ -1,4 +1,12 @@
 // Write a function expression that takes in a number and returns true if it's even and false if it's odd using arrow function
 
-const checkEvenOdd = number => ( number&1 )==0;
-console.log(checkEvenOdd(50));
+const checkEvenOdd = number => {
+    if(number===undefined)
+    {
+    return "No input provided";
+    }
+    return (number&1)==0; 
+
+}
+console.log(checkEvenOdd(50)); // input provided
+console.log(checkEvenOdd()); // no input provided
