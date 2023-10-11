5a265591994e288e74d9ea21eaa14265175bc320
munendra-successive <munendra.kushwaha@successive.tech>
2023-10-11 10:40:52 +0530

    Added proper Indentation

--- assignment-5/Question-4.js
+++ assignment-5/Question-4.js
@@ -1,44 +1,31 @@
 // Write a program deepClone that takes an object as input and returns a deep copy of that object.
 // The function should handle nested objects and arrays.
 
-
-const book={
-    title: "C++",
-    genre : "Programming",
-    publish:"1985",
-    greet:{
-        greet:{
-            greet:"Good morning",
-        }
+const book = {
+  title: "C++",
+  genre: "Programming",
+  publish: "1985",
+  greet: {
+    greet: {
+      greet: "Good morning",
     },
-    arr:[3,4,3,2,1]
- };
+  },
+  arr: [3, 4, 3, 2, 1],
+};
 
+const objcopy = function (input) {
+  if (typeof input !== "object") {
+    return input;
+  }
+  let copy = Array.isArray(input) ? [] : {};
+  for (key in input) {
+    const value = input[key];
+    copy[key] = objcopy(value);
+  }
+  return copy;
+};
 
- const objcopy = function(input)
- {
-    if(typeof input !== "object")
-    {
-        return input;
-    }
-    let copy = Array.isArray(input)?[]:{};
-    for(key in input)
-    {
-        const value = input[key];
-        copy[key]= objcopy(value);
-    }
-    return copy;
- }
-
-
- 
-
-var newobj =objcopy(book);
- book.greet.greet.greet="HEllo How are you";
- console.log(book);
- console.log(newobj);
-
-
-
-
-
+var newobj = objcopy(book);
+book.greet.greet.greet = "HEllo How are you";
+console.log(book);
+console.log(newobj);
--- assignment-5/Question-5.js
+++ assignment-5/Question-5.js
@@ -1,4 +1,3 @@
-
 /*
 Write a program flattenObject that takes an object with nested properties and returns a flat object with the nested properties transformed into dot-separated properties. The function should handle nested objects and arrays.
 const obj = {
@@ -8,36 +7,27 @@ const obj = {
 Output: { 'a': 1, 'b.c': 2, 'b.d.0': 3, 'b.d.1': 4 }
 */
 
-
-const obj ={
-    a:1,
-    b:{c:2, d:[3,[4,7,8]]},
+const obj = {
+  a: 1,
+  b: { c: 2, d: [3, [4, 7, 8]] },
 };
 
-
-const objcopy = function(obj,parent)
-{
-    const finobj ={};
-    const generateFlatObject = (obj, parent)=>{
-       for(let key in obj)
-       {
-        const newParent = parent+key;
-        const value = obj[key];
-        if(typeof value === "object")
-        {
-            generateFlatObject(value,newParent+".");
-        }
-        else{
-            finobj[newParent]=value;
-        }
-       }
+const objcopy = function (obj, parent) {
+  const finobj = {};
+  const generateFlatObject = (obj, parent) => {
+    for (let key in obj) {
+      const newParent = parent + key;
+      const value = obj[key];
+      if (typeof value === "object") {
+        generateFlatObject(value, newParent + ".");
+      } else {
+        finobj[newParent] = value;
+      }
     }
-    generateFlatObject(obj,parent);
+  };
+  generateFlatObject(obj, parent);
 
-    return finobj;
-}
+  return finobj;
+};
 
-
-console.log(objcopy(obj,""));
-console.log(obj);
-
+console.log(objcopy(obj, ""));
