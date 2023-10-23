972466ab6fcb8d2b731433bd255c31e89410c06b
munendra-successive <munendra.kushwaha@successive.tech>
2023-10-23 16:51:52 +0530

    modified the code of Question 8 and is running smoothly

--- assignment-7/Question-8.js
+++ assignment-7/Question-8.js
@@ -1,24 +1,24 @@
 /* Create a function that fetches data from multiple APIs in parallel and 
    then performs some operation on the combined data, using async/await.  */
 
-async function dataMultipleApi()
-{
-    const data1 = fetch("https://catfact.ninja/fact").then(response => response.json()).then(response=>response);
-    const data2 = fetch("https://catfact.ninja/fact").then(response=>response.json()).then(response=>response);
-    const data3 = fetch("https://catfact.ninja/fact").then(response=>response.json()).then(response=>response);
-    const response = await Promise.all([data1,data2,data3]);
-    return response;
+async function dataMultipleApi() {
+  const data1 = fetch("https://catfact.ninja/fact")
+    .then((response) => response.json())
+    .then((response) => response);
+  const data2 = fetch("https://catfact.ninja/fact")
+    .then((response) => response.json())
+    .then((response) => response);
+  const data3 = fetch("https://catfact.ninja/fact")
+    .then((response) => response.json())
+    .then((response) => response);
+  const response = await Promise.all([data1, data2, data3]);
+  return response;
 }
 
-dataMultipleApi().then(function(response){
-    let allFacts ="";
-    for(let index=0;index<response.length;index)
-    {
-        Object.keys(response[index]).forEach(function(item)
-        {
-            console.log(item.fact);
-        })
-            
-        
-    }
-})
+dataMultipleApi().then(function (response) {
+  let allFacts = "";
+  response.forEach((item, index) => {
+    allFacts += item.fact + "\n";
+  });
+  console.log(allFacts);
+});
