1a7ecea3d1531acbee6b3b50b207ccad6a53ef48
munendra-successive <munendra.kushwaha@successive.tech>
2023-10-23 10:07:57 +0530

    comments updated

--- assignment-6/Question-2.js
+++ assignment-6/Question-2.js
@@ -1,5 +1,5 @@
-// Create a class Shape with properties width and height and methods getArea(). Create two classes Rectangle and Triangle that inherit from the Shape class and
-// implement the getArea() method for their respective shapes.
+/* Create a class Shape with properties width and height and methods getArea(). Create two classes Rectangle and Triangle that inherit from the Shape class and
+implement the getArea() method for their respective shapes. */
 
 class Shape {
   constructor(width, height) {
